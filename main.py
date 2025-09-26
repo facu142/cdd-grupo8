aa0f7f41e82d456c40e5b62118e7932f971651ec
Facundo Montenegro <facumonte29@gmail.com>
2025-09-26 08:25:11 -0300

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import kagglehub
+import os
+import pandas as pd
 
 # Download latest version
 path = kagglehub.dataset_download("yashdevladdha/uber-ride-analytics-dashboard")
-
 print("Path to dataset files:", path)
+
+# List files in the downloaded folder
+files = os.listdir(path)
+print("Files in the dataset:", files)
+
+# Read a CSV file (for example, the first one)
+csv_file = [f for f in files if f.endswith('.csv')][0]
+df = pd.read_csv(os.path.join(path, csv_file))
+print(df.head())
+
